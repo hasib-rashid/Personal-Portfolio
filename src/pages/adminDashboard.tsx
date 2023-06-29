aab984ed213a6bf39c396cd7d7a30625397bb43b
Hasib Al Rashid <cadethasib3091@gmail.com>
2023-06-29 20:40:50 +0600

    🤯Made Admin Login Panel

--- src/pages/adminDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const links = () => {
-    return (
-        <div>links</div>
-    )
-}
-
-export default links
---
+++ src/pages/adminLogin.tsx
@@ -0,0 +1,148 @@
+import { useToggle, upperFirst, useLocalStorage } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
+import {
+    TextInput,
+    PasswordInput,
+    Text,
+    Paper,
+    Group,
+    PaperProps,
+    Button,
+    Divider,
+    Checkbox,
+    Anchor,
+    Stack,
+    ColorSchemeProvider,
+    MantineProvider,
+    createStyles,
+    rem,
+    Title,
+} from '@mantine/core';
+import { supabase } from "@/utils"
+import { useState } from 'react';
+
+export default function adminLogin(props: PaperProps) {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const form = useForm({
+        initialValues: {
+            email: '',
+            name: '',
+            password: '',
+            terms: true,
+        },
+
+        validate: {
+            email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+            password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+        },
+    });
+
+    const handleEmailChange = (e: any) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e: any) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        // Perform login logic with email and password
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
+    const useStyles = createStyles((theme) => ({
+        wrapper: {
+            minHeight: rem(900),
+            backgroundSize: 'cover',
+            backgroundImage:
+                'url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1280&q=80)',
+        },
+
+        form: {
+            borderRight: `${rem(1)} solid ${theme.colors.dark[7]}`,
+            minHeight: rem(900),
+            maxWidth: rem(450),
+            paddingTop: rem(80),
+
+            [theme.fn.smallerThan('sm')]: {
+                maxWidth: '100%',
+            },
+        },
+
+        title: {
+            color: theme.white,
+            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        },
+    }));
+
+    const [colorScheme, toggleColorScheme] = useLocalStorage({
+        key: 'mantine-color-scheme',
+        defaultValue: 'light',
+        getInitialValueInEffect: true,
+    });
+    const { classes } = useStyles();
+
+    const handleLogin = async () => {
+        console.log(form.values)
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: form.values.email,
+            password: form.values.password,
+        })
+        if (error) {
+            console.error('Error signing in:', error.message);
+        } else {
+            console.log('User:', data);
+        }
+    };
+
+    return (
+        // @ts-ignore
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            {/* @ts-ignore */}
+            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+                <div className={classes.wrapper}>
+                    <Paper className={classes.form} radius={0} p={30}>
+                        <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+                            Welcome back to Mantine!
+                        </Title>
+
+                        <form onSubmit={form.onSubmit(handleLogin)}>
+                            <Stack>
+                                <TextInput
+                                    required
+                                    label="Email"
+                                    placeholder="hello@mantine.dev"
+                                    value={form.values.email}
+                                    onChange={(event) => {
+                                        form.setFieldValue('email', event.currentTarget.value)
+                                    }}
+                                    error={form.errors.email && 'Invalid email'}
+                                    radius="md"
+                                />
+
+                                <PasswordInput
+                                    required
+                                    label="Password"
+                                    placeholder="Your password"
+                                    value={form.values.password}
+                                    onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+                                    error={form.errors.password && 'Password should include at least 6 characters'}
+                                    radius="md"
+                                />
+                            </Stack>
+
+                            <Group position="apart" mt="xl">
+                                <Button type="submit" radius="xl">
+                                    Login
+                                </Button>
+                            </Group>
+                        </form>
+                    </Paper>
+                </div>
+            </MantineProvider>
+        </ColorSchemeProvider>
+    );
+}
