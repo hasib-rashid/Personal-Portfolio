8b1f49918aeae8e4d668ad42acf4a7f323a65052
Hasib Al Rashid <cadethasib3091@gmail.com>
2024-03-27 16:46:42 +0600

    Updated Dashboard Security

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { supabase } from '@/utils';
-import React from 'react'
+import React, { useEffect } from 'react'
 import AdminHeader from '@/components/Admin/Header';
 import { useRouter } from 'next/navigation';
 
@@ -8,21 +8,24 @@ const Dashboard = () => {
     const router = useRouter()
     supabase.auth.getSession().then((res) => {
         if (!res.data.session) {
-            console.log("You're not logged in")
+            return (
+                <h1>404 Not found</h1>
+            )
+        } else {
+
+            return (
+                <div>
+                    <AdminHeader />
+                    <button onClick={() => {
+                        supabase.auth.signOut()
+                        router.push('/')
+                    }}>Sign Out</button>
+
+                    <h1>Dashboard</h1>
+                </div>
+            )
         }
     })
-
-    return (
-        <div>
-            <AdminHeader />
-            <button onClick={() => {
-                supabase.auth.signOut()
-                router.push('/')
-            }}>Sign Out</button>
-
-            <h1>Dashboard</h1>
-        </div>
-    )
 }
 
 export default Dashboard
