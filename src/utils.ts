9e10e436ca8e501cf55b54889e2d5cb1c7868d00
Hasib Al Rashid <hasibrashid3091@gmail.com>
2024-04-02 15:14:39 +0600

    Fixed Issue

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uwyucaypyleqwknagaxh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3eXVjYXlweWxlcXdrbmFnYXhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc1MTQwMzEsImV4cCI6MjAwMzA5MDAzMX0.ylpLq105bwbzYmG_wqlsqHmvATq5sKCBBaiWcLdKHQg';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+// @ts-ignore
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
